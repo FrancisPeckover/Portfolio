0bfdce86f832a7e20bfceb72f664063cafe7af2e
Francis Peckover <FrancisPeckover@users.noreply.github.com>
2020-09-08 16:35:11 +1000

    new design by britt

--- js/main.js
+++ js/main.js
@@ -1,42 +1,18 @@
-const darkTheme = [
-	'#282828',
-	'#6e6e6e',
-	'#ffffff',
-	'#008b8b'
-];
+let interceptAnims;
 
-const blueTheme = [
-	'#004E64',
-	'#00A5CF',
-	'#9FFFCB',
-	'#25A18E'
-];
-
-const lightTheme = [
-	'#A6A6A8',
-	'#E8E9F3',
-	'#CECECE',
-	'#272635'
-];
-
-function setTheme(x) {
-	switch (x) {
-		case 'blue':
-			changeTheme(blueTheme);
-			break;
-		case 'dark':
-			changeTheme(darkTheme);
-			break;
-		case 'light':
-			changeTheme(lightTheme);
-			break;
+document.addEventListener('DOMContentLoaded', () => {
+	interceptAnims = document.getElementsByClassName('animated');
+	for (let i = 0; i < interceptAnims.length; i++) {
+		interceptAnims[i].style.webkitAnimationPlayState = 'paused';
 	}
-}
+});
 
-function changeTheme(theme) {
-	let root = document.documentElement;
-	root.style.setProperty('--colour-primary', theme[0]);
-	root.style.setProperty('--colour-secondary', theme[1]);
-	root.style.setProperty('--colour-tertiary', theme[2]);
-	root.style.setProperty('--colour-accent', theme[3]);
+document.addEventListener('scroll', interceptAnim);
+
+function interceptAnim() {
+	for (let i = 0; i < interceptAnims.length; i++) {
+		if (window.pageYOffset + window.innerHeight > interceptAnims[i].offsetTop * 1.4) {
+			interceptAnims[i].style.webkitAnimationPlayState = 'running';
+		}
+	}
 }
